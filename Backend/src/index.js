0494a3f605adc88fcc991cb98770fc98feb72dc0
piyush <piyushjha150403@gmail.com>
2024-11-15 02:13:22 +0530

    vercel 2 changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,15 +2,18 @@ import "dotenv/config";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import dotenv from 'dotenv';
-
 dotenv.config();
+
 
 connectDB()
   .then(() => {
-    console.log(`⚙️ MongoDB connected successfully.`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+      console.log(
+        `🚀 Server is running at http://localhost:${process.env.PORT}/api/v1/`,
+      );
+    });
   })
   .catch((err) => {
     console.log("MongoDB Connection Failed!!", err);
   });
-
-export default app;  // Export the app directly for Vercel to use
