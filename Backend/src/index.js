fd4c7a2cf6dccadf26461aedf2680555e94cc634
piyush <piyushjha150403@gmail.com>
2024-11-15 15:05:49 +0530

    verceljson

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,16 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-      console.log(
-        `🚀 Server is running at http://localhost:${process.env.PORT}/api/v1/`,
-      );
+    app.listen(() => {
+      console.log("⚙️ Server is running on Vercel");
     });
+    
   })
   .catch((err) => {
     console.log("MongoDB Connection Failed!!", err);
